e9e82d98987c212b8d1caec40d0cb61cf85b4d01
LuisTorrez480 <a2025116705@estudiantes.upsa.edu.bo>
2026-04-29 12:30:00 -0400

    Implement ColaDoble class for double-ended queue

---
+++ ColaDoble.h
@@ -0,0 +1,22 @@
+// Ricardo Soruco 29/04/26
+#pragma once
+#include "NodoDoble.h"
+class ColaDoble
+{
+private:
+    Nodo* frente;
+    Nodo* final;
+
+public:
+    ColaDoble();
+
+    bool InsertarFrente(int dato);
+    bool InsertarFinal(int dato);
+    bool EliminarFrente();
+    bool EliminarFinal();
+
+    void VerFrente();
+    void VerCola();
+    void Tamanio();
+    bool Vacia();
+};
